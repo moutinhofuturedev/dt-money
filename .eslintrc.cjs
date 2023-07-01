f2877e6e3eb926506d7c3c4f6d12ff29aece9219
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-01 18:59:49 -0300

    style(master): ajuste config eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+}
