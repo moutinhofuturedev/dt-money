6e21b32cc46e6e608d5962ec5609f63f33d65201
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-05 07:41:55 -0300

    feature(DT-01): cria componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/Transactions'
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <p>Hello World</p>
+      <Transactions />
     </ThemeProvider>
   )
 }
