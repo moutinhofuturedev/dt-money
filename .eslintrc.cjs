da5e13cb66a8b2c1d4984c8ed1d018aa25ac3de1
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-06 23:28:42 -0300

    feature(DT-05): instala storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     '@rocketseat/eslint-config/react',
+    'plugin:storybook/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
