7c67ba08dddef44ed204343d03ffe01ef5f7b7a7
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-14 23:59:11 -0300

    feature(DT-03): cria tabela de valores

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  TransactionsContainer,
+  TransactionsTable,
+  PriceHighLight,
+} from './styles'
 
 export const Transactions = () => {
   return (
     <>
       <Header title="DT Money" name="Nova transação" />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel do apartamento</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
+              </td>
+              <td>Casa</td>
+              <td>13/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   )
 }
