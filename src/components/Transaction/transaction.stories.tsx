e49726c26904090d8ea42395fcba289089ef1c54
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-16 17:01:59 -0300

    feature(DT-03): cria storybook de transaction

---
+++ src/components/Transaction/transaction.stories.tsx
@@ -0,0 +1,30 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Transaction } from '.'
+
+const meta = {
+  title: 'Components/Transaction',
+  component: Transaction,
+  tags: ['autodocs'],
+} as Meta<typeof Transaction>
+
+export default meta
+
+export const Rendas: StoryObj<typeof Transaction> = {
+  args: {
+    tableTitle: 'Salário',
+    tableValue: 'R$ 5.400,00',
+    tableType: 'Salário',
+    tableDate: '06/04/2023',
+    typeExpense: 'income',
+  },
+}
+
+export const Despesas: StoryObj<typeof Transaction> = {
+  args: {
+    tableTitle: 'Contas',
+    tableValue: '- R$ 5.400,00',
+    tableType: 'Contas',
+    tableDate: '06/04/2023',
+    typeExpense: 'outcome',
+  },
+}
