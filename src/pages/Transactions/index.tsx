8b8af9146f650a6cfb5da99d163350688468efea
Paulo Moutinho <paulo_vicali@icloud.com>
2023-07-16 17:02:49 -0300

    feature(DT-03): chama importação de transaction

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,39 +1,26 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import {
-  TransactionsContainer,
-  TransactionsTable,
-  PriceHighLight,
-} from './styles'
+import { Transaction } from '../../components/Transaction'
 
 export const Transactions = () => {
   return (
     <>
       <Header title="DT Money" name="Nova transação" />
       <Summary />
-
-      <TransactionsContainer>
-        <TransactionsTable>
-          <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2023</td>
-            </tr>
-            <tr>
-              <td width="50%">Aluguel do apartamento</td>
-              <td>
-                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
-              </td>
-              <td>Casa</td>
-              <td>13/04/2023</td>
-            </tr>
-          </tbody>
-        </TransactionsTable>
-      </TransactionsContainer>
+      <Transaction
+        typeExpense="income"
+        tableTitle="Desenvolvimento de site"
+        tableValue="R$ 12.000,00"
+        tableType="Venda"
+        tableDate="12/12/2022"
+      />
+      <Transaction
+        typeExpense="outcome"
+        tableTitle="Aluguel do apartamento"
+        tableValue="- R$ 1.200,00"
+        tableType="Casa"
+        tableDate="01/04/2023"
+      />
     </>
   )
 }
