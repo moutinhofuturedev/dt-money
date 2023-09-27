bed76465f9d4ed572be5cc0be3ec6462dadececb
Paulo Moutinho <paulo_vicali@icloud.com>
2023-09-27 12:44:54 -0300

    refactor(conect_api): resolve

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
 import { Transactions } from './pages/Transactions'
-import { TransactionsProvider } from './context/TransactionContext'
+import { TransactionsProvider } from './contexts/TransactionContext'
 
 const App = () => {
   return (
