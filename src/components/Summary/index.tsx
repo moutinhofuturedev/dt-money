6170b4313d50674ed2da7edc92dee688f1e4ce4f
Paulo Moutinho <paulo_vicali@icloud.com>
2023-10-03 00:07:27 -0300

    feat(developer-01): utiliza reducer para calculo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,26 +2,49 @@ import { SummaryCard } from './SummaryCards'
 import { SummaryContainer } from './styles'
 import { ArrowUpCircle, ArrowDownCircle, DollarSign } from 'lucide-react'
 
+import { useContext } from 'react'
+import { TransactionsContext } from '../../contexts/TransactionContext'
+
 export const Summary = () => {
+  const { transactions } = useContext(TransactionsContext)
+
+  const { income, outcome, total } = transactions.reduce(
+    (accumulator, transaction) => {
+      if (transaction.expense === 'income') {
+        accumulator.income += transaction.price
+        accumulator.total += transaction.price
+      } else {
+        accumulator.outcome += transaction.price
+        accumulator.total -= transaction.price
+      }
+
+      return accumulator
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
   return (
     <SummaryContainer>
       <SummaryCard
         coloring="gray"
         headerType="Entradas"
         icon={<ArrowUpCircle size={32} color="#00B37E" />}
-        values="R$ 17.400,00"
+        values={income}
       />
       <SummaryCard
         coloring="gray"
         headerType="Saídas"
         icon={<ArrowDownCircle size={32} color="#F75A68" />}
-        values="R$ 8.259,00"
+        values={outcome}
       />
       <SummaryCard
         coloring="green"
         headerType="Total"
         icon={<DollarSign size={32} color="#FFFFFF" />}
-        values="R$ 9.141,00"
+        values={total}
       />
     </SummaryContainer>
   )
