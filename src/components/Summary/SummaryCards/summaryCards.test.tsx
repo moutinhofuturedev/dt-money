3919dee2ec478e72b6b18bd1ef06ffbebc45b590
Paulo Moutinho <paulo_vicali@icloud.com>
2023-10-15 22:50:05 -0300

    test(develop-01): atualiza testes

--- src/components/Summary/SummaryCards/summaryCards.test.tsx
+++ src/components/Summary/SummaryCards/summaryCards.test.tsx
@@ -3,23 +3,21 @@ import { render, screen } from '@testing-library/react'
 import { SummaryCard } from '.'
 
 describe('SummaryCard', () => {
-  it('renders header and values correctly', () => {
+  it('renders header and price correctly', () => {
     render(
-      <SummaryCard headerType="Header" values="123" icon={<div>Icon</div>} />,
+      <SummaryCard headerType="Header" price={123.12} icon={<div>Icon</div>} />,
     )
 
     const textHeader = screen.getByText('Header')
-    const textValues = screen.getByText('123')
     const showIcon = screen.getByText('Icon')
 
     expect(textHeader).toBeInTheDocument()
-    expect(textValues).toBeInTheDocument()
     expect(showIcon).toBeInTheDocument()
   })
 
   it('does not apply coloring when not specified', () => {
     const { container } = render(
-      <SummaryCard headerType="Header" values="123" icon={<div>Icon</div>} />,
+      <SummaryCard headerType="Header" price={123} icon={<div>Icon</div>} />,
     )
 
     expect(container.firstChild).not.toHaveClass('green')
