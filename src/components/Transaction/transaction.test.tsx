8faac8876a53247e00dd3b1c90f90781bc85e84d
Paulo Moutinho <paulo_vicali@icloud.com>
2023-10-15 23:13:00 -0300

    test(develop-01): nova data

--- src/components/Transaction/transaction.test.tsx
+++ src/components/Transaction/transaction.test.tsx
@@ -1,22 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import { Transaction, TransactionTableProps } from '.'
+import { dateFormatter } from '../../utils/formatter'
 
 describe('Transaction', () => {
   const mockTransaction: TransactionTableProps = {
     tableDescription: 'Item A',
     tablePrice: 100.0,
     tableCategory: 'Category',
-    tableDate: '12/08/2023',
+    tableDate: '2023-09-24T02:00:29.871Z',
     tableExpense: 'outcome',
   }
 
   it('renders the transaction details correctly', () => {
     render(<Transaction {...mockTransaction} />)
 
-    const formattedDate = new Date(
-      mockTransaction.tableDate,
-    ).toLocaleDateString()
-
+    const formattedDate = dateFormatter.format(
+      new Date(mockTransaction.tableDate),
+    )
     const description = screen.getByText(mockTransaction.tableDescription)
     const price = screen.getByText(
       new RegExp(mockTransaction.tablePrice.toString()),
