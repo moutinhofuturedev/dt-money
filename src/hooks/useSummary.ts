996f91e0abc998069dbb73a905050529123392f3
Paulo Moutinho <paulo_vicali@icloud.com>
2023-10-22 00:57:09 -0300

    refactor(add-axios): altera condicional

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -4,26 +4,26 @@ import { TransactionsContext } from '../context/TransactionContext'
 export const useSummary = () => {
   const { transactions } = useContext(TransactionsContext)
 
-  const summary =
-    transactions &&
-    transactions.reduce(
-      (accumulator, transaction) => {
-        if (transaction.expense === 'income') {
-          accumulator.income += transaction.price
-          accumulator.total += transaction.price
-        } else {
-          accumulator.outcome += transaction.price
-          accumulator.total -= transaction.price
-        }
+  const summary = transactions
+    ? transactions.reduce(
+        (accumulator, transaction) => {
+          if (transaction.expense === 'income') {
+            accumulator.income += transaction.price
+            accumulator.total += transaction.price
+          } else {
+            accumulator.outcome += transaction.price
+            accumulator.total -= transaction.price
+          }
 
-        return accumulator
-      },
-      {
-        income: 0,
-        outcome: 0,
-        total: 0,
-      },
-    )
+          return accumulator
+        },
+        {
+          income: 0,
+          outcome: 0,
+          total: 0,
+        },
+      )
+    : null
 
   return summary
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -13,19 +13,20 @@ export const Transactions = () => {
       <Header title="DT Money" name="Nova transação" />
       <Summary />
       <SearchForm />
-      {transactions &&
-        transactions.map((transaction) => {
-          return (
-            <Transaction
-              key={transaction.id}
-              tableExpense={transaction.expense}
-              tableDescription={transaction.description}
-              tablePrice={transaction.price}
-              tableCategory={transaction.category}
-              tableDate={transaction.createdAt}
-            />
-          )
-        })}
+      {transactions
+        ? transactions.map((transaction) => {
+            return (
+              <Transaction
+                key={transaction.id}
+                tableExpense={transaction.expense}
+                tableDescription={transaction.description}
+                tablePrice={transaction.price}
+                tableCategory={transaction.category}
+                tableDate={transaction.createdAt}
+              />
+            )
+          })
+        : null}
     </>
   )
 }
