a090aeb701105a6f82bf610417531be592c91532
Paulo Moutinho <paulo_vicali@icloud.com>
2023-10-28 00:29:29 -0300

    feat(register): cria função de post

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useContext } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import {
   Overlay,
@@ -7,16 +8,21 @@ import {
   TransactionTypeButton,
 } from './styles'
 import { ArrowDownCircle, ArrowUpCircle, X } from 'lucide-react'
+import { Toaster, toast } from 'react-hot-toast'
 
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 
+import { api } from '../../lib/api'
+import axios, { AxiosError } from 'axios'
+import { TransactionsContext } from '../../context/TransactionContext'
+
 const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  type: z.enum(['income', 'outcome']),
+  expense: z.enum(['income', 'outcome']),
 })
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
@@ -27,24 +33,70 @@ export const NewTransactionModal = () => {
     register,
     handleSubmit,
     formState: { isSubmitting },
+    reset,
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
-    defaultValues: { type: 'income' },
+    defaultValues: { expense: 'income' },
   })
 
-  const handleCreateNewTransaction = async (data: NewTransactionFormInputs) => {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+  const { fetchTransactions } = useContext(TransactionsContext)
 
-      console.log(data)
-    } catch (error) {
-      console.error('An error occurred:', error)
-    }
-  }
+  const handleCreateNewTransaction = useCallback(
+    async (data: NewTransactionFormInputs) => {
+      const { description, price, category, expense } = data
+
+      try {
+        await api.post('/transactions', {
+          description,
+          expense,
+          category,
+          price,
+          createdAt: new Date(),
+        })
+
+        reset()
+
+        if (fetchTransactions) {
+          await fetchTransactions()
+
+          toast.success('Transação cadastrada', {
+            duration: 5000,
+            position: 'top-left',
+          })
+        }
+      } catch (error) {
+        toast.error('Não foi possível cadastrar transação', {
+          duration: 5000,
+          position: 'top-left',
+        })
+
+        if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError
+
+          if (axiosError.response) {
+            console.log(
+              'Erro na resposta da API:',
+              axiosError.response.status,
+              axiosError.response.data,
+            )
+          } else if (axiosError.request) {
+            console.log('Erro na solicitação da API:', axiosError.request)
+          } else {
+            console.log(
+              'Erro ao configurar a solicitação da API:',
+              axiosError.message,
+            )
+          }
+        }
+      }
+    },
+    [fetchTransactions, reset],
+  )
 
   return (
     <Dialog.Portal>
       <Overlay />
+      <Toaster />
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
         <CloseButton>
@@ -72,7 +124,7 @@ export const NewTransactionModal = () => {
 
           <Controller
             control={control}
-            name="type"
+            name="expense"
             render={({ field }) => {
               return (
                 <TransactionType
